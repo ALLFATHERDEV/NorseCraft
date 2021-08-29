d8714fd14ab55cdb94ce08150b16e6f993119dde
Tobias <tobias.sauer207@gmail.com>
2021-08-29 19:22:44 +0200

    test

--- src/main/java/com/norsecraft/NorseCraftMod.java
+++ src/main/java/com/norsecraft/NorseCraftMod.java
@@ -30,6 +30,7 @@ public class NorseCraftMod implements ModInitializer {
         multiblockManager = new MultiblockManager();
         GeckoLib.initialize();
 
+        
         NCBlocks.register();
         NCItems.register();
         NCEntities.register();
