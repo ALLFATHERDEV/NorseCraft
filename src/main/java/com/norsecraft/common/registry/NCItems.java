92d798a951164c127eb6ba44fa4c7fc402957f91
Cash <master_cash@live.com>
2021-10-18 00:04:11 -0500

    add coin items

--- src/main/java/com/norsecraft/common/registry/NCItems.java
+++ src/main/java/com/norsecraft/common/registry/NCItems.java
@@ -13,12 +13,20 @@ public class NCItems {
 
     public static final Item ROCK = new Item(defSettings());
     public static final Item CAMPFIRE_STAND = new CampfireStandItem(defSettings());
+    public static final Item COPPER_COIN = new Item(defSettings());
+    public static final Item SILVER_COIN = new Item(defSettings());
+    public static final Item GOLD_COIN = new Item(defSettings());
+    public static final Item COIN_PURSE = new Item(defSettings().maxCount(1));
 
     public static void register() {
         NorseCraftMod.LOGGER.info("Register items");
 
         registerItem("rock", ROCK);
         registerItem("campfire_stand", CAMPFIRE_STAND);
+        registerItem("copper_coin", COPPER_COIN);
+        registerItem("silver_coin", SILVER_COIN);
+        registerItem("gold_coin", GOLD_COIN);
+        registerItem("coin_purse", COIN_PURSE);
     }
 
     public static <T extends Item> void registerItem(String name, T item) {
